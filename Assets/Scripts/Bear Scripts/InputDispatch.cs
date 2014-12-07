9d298cef1db3dab4bffe839cc928b3b303315477
Ian Dunbar <idunbar@soe.ucsc.edu>
2014-12-07 13:52:18 -0800

    Added a new scene for a new Bear
    Added a new scene so that I am not messing around in the old one, even
    though it is likely that we will soon be canning that scene.

---
+++ Assets/Scripts/Bear Scripts/InputDispatch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputDispatch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
