b7fe33f7c3a9f7731fb35ce85a9360bbb8798604
Ian Dunbar <idunbar@soe.ucsc.edu>
2014-12-07 23:58:46 -0800

    New Camera Script!
    I got tired of the old camera script so I made a simple new one. Its not
    that great but it can be improved

---
+++ Assets/Scripts/Camera Scripts/SimpleFollow.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleFollow : MonoBehaviour {
+
+    //////////////////////////////////////
+    // Private Variables
+    //////////////////////////////////////
+
+    private Transform transRef;
+
+    //////////////////////////////////////
+
+
+    //////////////////////////////////////
+    // Inspector Variables
+    //////////////////////////////////////
+    
+    [SerializeField]    // The target for the object to follow
+    private Transform   followTarget;
+    [SerializeField]    // The target offset for the following object2
+    private Vector3     targetOffset;
+    [SerializeField]    // The maximum speed that the camera can move
+    private float       maxSpeed;
+    [SerializeField]    // Whether or not the update should happen in the Fixed update step
+    private bool        updateOnFixed;
+
+    //////////////////////////////////////
+
+
+    //////////////////////////////////////
+    // Unity Message Handling
+    //////////////////////////////////////
+
+	// Use this for initialization
+	void Awake ( ) 
+    {
+        // Get the reference to this transform for future use
+        transRef = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ( ) 
+    {
+        if( !updateOnFixed )
+        {
+            transRef.position = Vector3.MoveTowards( transRef.position, followTarget.position + targetOffset, maxSpeed * Time.deltaTime);
+        }
+	}
+
+    void FixedUpdate( )
+    {
+        if( updateOnFixed )
+        {
+            transRef.position = Vector3.MoveTowards( transRef.position, followTarget.position + targetOffset, maxSpeed * Time.fixedDeltaTime );
+        }
+
+    }
+
+    //////////////////////////////////////
+}
