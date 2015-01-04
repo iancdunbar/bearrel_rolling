bbeea87fcf0e379998c5cb5df3c62f0883a80057
Ian Dunbar <idunbar@soe.ucsc.edu>
2015-01-04 15:38:14 -0800

    Adding the 2D Scene
    And cleaning up some of the old stuff, and testing the slack integration
    at the same time

---
+++ Assets/Scripts/2D_Scripts/Camera Scripts/Simple2DFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simple2DFollow : MonoBehaviour {
+
+    /////////////////////////////////////////////
+    // Private Variable
+    /////////////////////////////////////////////
+
+    Transform trans_ref;
+
+    /////////////////////////////////////////////
+
+
+    /////////////////////////////////////////////
+    // Inspector Variables
+    /////////////////////////////////////////////
+
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    Vector2 offset;
+
+    /////////////////////////////////////////////
+
+
+
+	// Use this for initialization
+	void Awake () 
+    {
+        trans_ref = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        trans_ref.position = new Vector3( target.position.x + offset.x, target.position.y + offset.y, trans_ref.position.z );
+	}
+}
