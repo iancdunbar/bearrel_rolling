83f39220185c893bad22017a2aeb1e13c5a23281
TensAndTwenties <robbie.carter@gmail.com>
2015-02-08 07:36:14 -0800

    New Scene: 3d bear, 2d collisions

---
+++ Assets/Scripts/Bear Scripts/PinTransformToTarget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinTransformToTarget : MonoBehaviour {
+
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	public Transform ColliderContainer;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = this.transform.position;
+		newPosition.z = 0f;
+		newPosition.x = ColliderContainer.transform.position.x;
+		newPosition.y = ColliderContainer.transform.position.y;
+
+
+
+		this.transform.position = newPosition;
+	}
+}
