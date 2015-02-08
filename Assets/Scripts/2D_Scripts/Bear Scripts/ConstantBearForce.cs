246f86bc9e48fc4fa0e4e647d38929fc556c06ba
TensAndTwenties <robbie.carter@gmail.com>
2015-02-08 11:09:59 -0800

    New Scene: 2d bear, 2d collsions

---
+++ Assets/Scripts/2D_Scripts/Bear Scripts/ConstantBearForce.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantBearForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (5f * Time.deltaTime, 0, 0, Space.World);
+	}
+}
