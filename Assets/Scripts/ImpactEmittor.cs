6da4da9111d96482e205381ceda2e962d94bf7cc
Jared Mills <jared.mills23@yahoo.com>
2015-02-22 09:08:15 -0800

    tried to get impact partial system

---
+++ Assets/Scripts/ImpactEmittor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImpactEmittor : MonoBehaviour {
+
+	// Use this for initialization
+	public ParticleSystem snow;
+	
+	void OnCollisionEnter2D (Collider2D col){
+		
+		if(col.gameObject.tag == "Ground"){
+			Debug.Log ("Collision Occured");
+			snow.Play ();
+
+			
+		}
+		
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
