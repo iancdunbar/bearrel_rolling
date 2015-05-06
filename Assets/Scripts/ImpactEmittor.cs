d7fd9eb9f6caae27fdcc33a1bfd277e500e2726c
Jared Mills <jared.mills23@yahoo.com>
2015-05-05 18:33:14 -0700

    Camera work
    Implemented a base moving camera position

--- Assets/Scripts/ImpactEmittor.cs
+++ Assets/Scripts/ImpactEmittor.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class ImpactEmittor : MonoBehaviour {
-
+	
 	// Use this for initialization
 	public ParticleSystem snow;
 	public ParticleSystem Impact;
-    public bool can_blood = true;
+	public bool can_blood = true;
 	public bool grounded = false;
 	public ParticleSystem ContactEmitter;
 	public ParticleSystem MovingFastEmitter;
@@ -31,14 +31,14 @@ public class ImpactEmittor : MonoBehaviour {
 	private Vector3 originalOffset;
 	private float startTime;
 	public float resetTime = 1;
-
-
+	
+	
 	void Awake () 
 	{
 		bc = GameObject.Find ("Bear_Body").GetComponent<BearController>();
-
+		
 	}
-
+	
 	void Start ()
 	{	
 		startTime = Time.time;
@@ -47,8 +47,8 @@ public class ImpactEmittor : MonoBehaviour {
 		MessageDispatch.RegisterListener( "OnEnterState", OnEnterState );
 		MessageDispatch.RegisterListener( "OnExitState", OnExitState );
 	}
-
-
+	
+	
 	void OnEnterState ( object arg){
 		BearState state = (BearState)arg;
 		if (state == BearState.SLAMMING){
@@ -59,11 +59,11 @@ public class ImpactEmittor : MonoBehaviour {
 			jumping = true;
 		}
 	}
-
+	
 	void OnExitState (object arg){
 		BearState  state = (BearState)arg;
 		if (state == BearState.SLAMMING){
-
+			
 		}
 	}
 	void Update () 
@@ -71,21 +71,21 @@ public class ImpactEmittor : MonoBehaviour {
 		bearspeed = GetComponent<Rigidbody2D>().velocity;
 		bearpos = GetComponent<Rigidbody2D>().position;
 		//CLAMP the min max value of the bears velocity for particle speed
-
+		
 		ContactClamp = Mathf.Clamp (GetComponent<Rigidbody2D>().velocity.x, 4,14);
 		MovingFastClamp = Mathf.Clamp (GetComponent<Rigidbody2D>().velocity.x, 0,1f);
 		RotationClamp = Mathf.Clamp (GetComponent<Rigidbody2D>().velocity.x, 0, 0.1f);
-
+		
 		//ContactEmitter.startSpeed = ContactClamp;
 		//ContactEmitter.startSize = MovingFastClamp;
 		MovingFastEmitter.startSpeed = ContactClamp;
-
-
-
-
-
+		
+		
+		
+		
+		
 		//SLAMMING VFX //////
-
+		
 		if (slamming == true)
 		{
 			slam.Play ();
@@ -95,13 +95,13 @@ public class ImpactEmittor : MonoBehaviour {
 		{
 			slam.Stop ();
 		}
-
-
-
-
-
+		
+		
+		
+		
+		
 		//DASH VFX //////
-
+		
 		if (bc.dashed == true)
 		{
 			dash.Play ();
@@ -110,58 +110,62 @@ public class ImpactEmittor : MonoBehaviour {
 		{
 			dash.Stop ();
 		}
-
-
-		//Is the bear moving fast?
-		if (GetComponent<Rigidbody2D>().velocity.x >= MovingFastSpeed){
-			movingfast = true;
-		}
-		else {
-			movingfast = false;
-		}
-
-
-
-
-
-
+		
+		
+		
+		
+		
+		
+		
+		
 		//CONTACT EMITTER (Rooster Tail) ////// 
-
+		
 		//Emit particles if the bear is touching the ground while moving fast
 		if(grounded == true && movingfast == true){
 			ContactEmitter.Play();
 		}
 		else { 
 			ContactEmitter.Stop();
-		
+			
 		}
 		//The contact emitter shouldn't be running if the bear moves off of the terrain
 		if (jumping == true)
 		{
 			ContactEmitter.Stop ();
 		}
-
-
-
-
+		
+		
+		//FAST MOVEMENT //
+		if (GetComponent<Rigidbody2D>().velocity.x >= MovingFastSpeed)
+		{
+			movingfast = true;
+			mCamera.GetComponent<GameCamera>().offset = Vector3.Slerp (mCamera.GetComponent<GameCamera>().offset, mCamera.GetComponent<GameCamera>().MovingOffset, 0.008f);
+		}
+		else 
+		{
+			mCamera.GetComponent<GameCamera>().offset = Vector3.Slerp (mCamera.GetComponent<GameCamera>().offset, originalOffset, 0.008f);
+			movingfast = false;
+		}
+		
+		
 		//REALLY FAST MOVEMENT//
-
+		
 		if (GetComponent<Rigidbody2D>().velocity.x >= ReallyFastSpeed.x && GetComponent<Rigidbody2D>().velocity.y <= ReallyFastSpeed.y){
 			reallyfast = true;
-
+			
 			//if Bear is moving REALLY FAST zoom the camera out and give the bear a slight lead
 			mCamera.GetComponent<GameCamera>().offset = Vector3.Slerp (mCamera.GetComponent<GameCamera>().offset, mCamera.GetComponent<GameCamera>().FastOffset, 0.008f);
-
+			
 		}
 		else {
-
-			mCamera.GetComponent<GameCamera>().offset = Vector3.Slerp (mCamera.GetComponent<GameCamera>().offset, originalOffset, 0.008f);
+			
+			mCamera.GetComponent<GameCamera>().offset = Vector3.Slerp (mCamera.GetComponent<GameCamera>().offset, mCamera.GetComponent<GameCamera>().MovingOffset, 0.008f);
 			reallyfast = false;
-
+			
 		}
-
-
-		//Emit TRAILS if the bear is moving REALLY fast ANYTIME in the air.
+		
+		
+		//Emit TRAILS if the bear is moving REALLY fast ANYTIME
 		if (reallyfast == true)
 		{
 			Trails = GetComponentsInChildren<TrailRenderer>();
@@ -171,16 +175,16 @@ public class ImpactEmittor : MonoBehaviour {
 				Trail.enabled = true;
 				Trail.time = 0.3f;
 			}
-
-
+			
+			
 		}
 		else
 		{
 			if (grounded == false)
 			{
-			Trails = GetComponentsInChildren<TrailRenderer>();
-			
-			foreach (TrailRenderer Trail in Trails) 
+				Trails = GetComponentsInChildren<TrailRenderer>();
+				
+				foreach (TrailRenderer Trail in Trails) 
 				{
 					Trail.enabled = true;
 					Trail.time = 0.05f;
@@ -199,7 +203,7 @@ public class ImpactEmittor : MonoBehaviour {
 		}
 	}
 	
-
+	
 	//TREE COLLISION//
 	void OnTriggerEnter2D(Collider2D other)
 	{
@@ -209,42 +213,42 @@ public class ImpactEmittor : MonoBehaviour {
 			//tell the GameCamera script to shake the cam
 			//mCamera.GetComponent<GameCamera>().Shake = true;
 		}
-
-
+		
+		
 		if ( other.tag == "tree")
 		{
 			//Ping VFX if tree is collided with
 			Instantiate( Impact, transform.position, Quaternion.identity );
 			//Momentarily interrupt contact vfx if collided with tree.
-
-
+			
+			
 		}
 		if (other.tag == "Rock")
 		{
 			Instantiate( Impact, transform.position, Quaternion.identity );
 			mCamera.GetComponent<GameCamera>().Shake = true;
 		}
-
-        can_blood = true;
-
-
-
-
-
-			
+		
+		can_blood = true;
+		
+		
+		
+		
+		
+		
 	}
-
-
-    void OnCollisionEnter2D( Collision2D other )
-    {
-
-
-
+	
+	
+	void OnCollisionEnter2D( Collision2D other )
+	{
+		
+		
+		
 		grounded = true;
 		jumping = false;
 		slam.Stop ();
-
-
+		
+		
 		if( slamming == true && can_blood )
 		{
 			
@@ -258,31 +262,31 @@ public class ImpactEmittor : MonoBehaviour {
 			can_blood = false;
 			slamming = false;
 			
-
+			
 		}
 		if( can_blood )
-        {
-            
-            Vector3 pos = other.contacts[0].point;
-            pos.z = transform.position.z;
-
-            Instantiate( snow, pos, Quaternion.identity );
-            SimpleAudioController.PlayCrashEmote( );
-
-            can_blood = false;
-
-        }
-
-    }
+		{
+			
+			Vector3 pos = other.contacts[0].point;
+			pos.z = transform.position.z;
+			
+			Instantiate( snow, pos, Quaternion.identity );
+			SimpleAudioController.PlayCrashEmote( );
+			
+			can_blood = false;
+			
+		}
+		
+	}
 	void OnCollisionExit2D( Collision2D other)
 	{
-
-
-			grounded = false;
-
-
+		
+		
+		grounded = false;
+		
+		
 	}
-
-
-
+	
+	
+	
 }
